ab21584f465a4a44738138e971ec3ea6c1d62d8d
Lisa Schaefer <crowdpublishtv@gmail.com>
2014-09-22 22:21:34 -0400

    Take sprockets out of app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require shadowbox
-//= require turbolinks
 //= require_tree .
 $('.dropdown-toggle').dropdown() 
