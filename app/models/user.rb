72017531ec1f5481b6ab58f24d9dc65c97156b70
Lisa Schaefer <crowdpublishtv@gmail.com>
2014-09-30 22:08:49 -0400

    Got rid of password confirmation requirement for updating user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 },  :on => :create
   validates_acceptance_of :terms_of_service
   
   validates :name, :uniqueness => true
