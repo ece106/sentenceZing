e2d5d4048c5cf9abc87cff6b5c294ac5b4ea25a1
mwatkin3 <mwatkin3@masonlive.gmu.edu>
2014-12-16 21:39:48 -0500

    Fixed Story Refresh js and removed chatango completely

--- app/assets/javascripts/story.js
+++ app/assets/javascripts/story.js
@@ -1,11 +1,31 @@
 $(document).on("page:change", function() {
-  var $storyid = $('.storyfeed').data('story-id'); 
-//  alert("started story.js " + $('.storyfeed').data('story-turn') );
-//    $.getScript('/stories.js?story_turn=' + $turn + "&turnchange=" + $turnchange );
-  $.getScript('/microposts.js?story_id=' + $storyid );
+	
+	var urlArray = window.location.href.split( '/' );
+	if (urlArray[3] == "stories" && urlArray[5] == "edit")
+	{
+		var $currentTurn = $('.storyfeed').data('story-turn');
+		var $currentUser = $('.storyfeed').data('curruser');
+		
+		if($currentUser == $currentTurn)
+		{
+			var $storyid = $('.storyfeed').data('story-id'); 
+			//  alert("started story.js " + $('.storyfeed').data('story-turn') );
+			//    $.getScript('/stories.js?story_turn=' + $turn + "&turnchange=" + $turnchange );
+  
+			$.getScript('/microposts.js?story_id=' + $storyid );
  
-  function refreshStories(){
-    $.getScript('/microposts.js?story_id=' + $storyid);
-  };
-  setInterval(refreshStories, 10000);
+			function refreshStories(){
+				$.getScript('/microposts.js?story_id=' + $storyid);
+			};
+			setInterval(refreshStories, 20000);
+		}
+		else
+		{
+			return;
+		}
+	}
+	else
+	{
+		return;
+	}
 }); 
