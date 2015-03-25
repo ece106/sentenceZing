cbda4502c5b0e6cda4de571288d744d72f6c2679
Lisa Schaefer <crowdpublishtv@gmail.com>
2015-03-24 21:25:14 -0400

    Don't have to shh anymore. Put security protocols back into users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-#    before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
-#    before_action :correct_user,   only: [:edit, :update]
+    before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+    before_action :correct_user,   only: [:edit, :update]
 
     # GET /users
     # GET /users.json
